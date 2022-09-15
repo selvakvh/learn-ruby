1a499e5619381048ea908cc2ad897909d43b9bf0
Selvarasu Ramasamy <sramasamy@kvh.com>
2022-09-15 08:45:06 +0530

    ch 5 exercise

--- functions-and-blocks/exercise.rb
+++ functions-and-blocks/exercise.rb
@@ -1,3 +1,10 @@
 99.downto(1) do |number|
   puts "#{number} bottle#{number == 1 ? '' : 's'} on the wall"
 end
+
+def email_parts(email)
+  email.downcase.split('@')
+end
+
+
+puts email_parts('selva.home@gmail.com')
