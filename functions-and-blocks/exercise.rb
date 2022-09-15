629ce383c7f4e3ad13b724d31e2e5c98964386e9
Selvarasu Ramasamy <sramasamy@kvh.com>
2022-09-15 08:49:19 +0530

    ch 5 exercise

--- functions-and-blocks/exercise.rb
+++ functions-and-blocks/exercise.rb
@@ -6,5 +6,11 @@ def email_parts(email)
   email.downcase.split('@')
 end
 
-
 puts email_parts('selva.home@gmail.com')
+
+def palindrome?(string)
+  string.downcase!
+  string == string.reverse
+end
+
+puts palindrome?('Racecar')
