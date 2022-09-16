ddc5bd9395206f803faae9f60baae6d8c76166ba
Selvarasu Ramasamy <sramasamy@kvh.com>
2022-09-16 08:13:05 +0530

    add test framework

---
+++ web-app/test/site_pages_test.rb
@@ -0,0 +1,14 @@
+require_relative 'test_helper'
+
+class PalindromAppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get '/'
+    assert last_response.ok?
+  end
+end
